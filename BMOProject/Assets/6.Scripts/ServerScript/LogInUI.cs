8bd0dbc686cb9d910c1a392e9647a8f3a36c7230
FriedenSehnsucht37 <169035573+schwarzhans37@users.noreply.github.com>
2024-05-25 22:22:01 +0900

    프로젝트 분할 업로드1

---
+++ BMOProject/Assets/6.Scripts/ServerScript/LogInUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class LogInUI : MonoBehaviour
+{
+    [SerializeField]
+    private InputField UserID_Field;
+    [SerializeField]
+    private InputField UserPW_Field;
+    public GameObject LogInButton;
+
+}

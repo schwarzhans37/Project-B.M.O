a78f9d7014edc766bef44851d223d57dd31aa43e
Abdullah2647 <chosh9905@naver.com>
2024-08-05 15:29:47 +0900

    a
    a

--- TestTeamProject/Assets/6.Scripts/Player/PlayerController.cs
+++ TestTeamProject/Assets/6.Scripts/Player/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
 
     //캐릭터 걷기 이동속도            
     [SerializeField] float WalkSpeed = 2f;    
-    [SerializeField] float RunSpeed = 4f; 
+    [SerializeField] float RunSpeed = 3f; 
     [SerializeField] float JumpForce = 5f;
 
     //캐릭터 애니메이션 컴포넌트
